3e365f701f5f8de4229604ff97b0464e7bcdd3a5
Kenneth Ma <ken@kenma.ca>
2017-05-02 01:22:50 -0400

    Fixed: Up and down auto scrolling

--- js/main.js
+++ js/main.js
@@ -10,8 +10,6 @@ $(() => {
       triggerHook: 'onLeave'
     }
   });
-  var tw = TweenMax;
-  var tl = TimelineMax;
 
   // first scene: ferris wheel rotation
   tw.to($('g[class*="wheel"], #cars'), FERRIS_SPEED, {rotation: ROTATION, repeat: -1, transformOrigin: '50% 50%', ease: Linear.easeNone});
@@ -26,15 +24,15 @@ $(() => {
     triggerElement: '#first',
     duration: 0,
     offset: 10
-  }).setTween(new tl()
-    .to($('#banner'), 0.5, {opacity: 0})
+  }).setTween(new tl({
+    onComplete: jump('#second')
+  }).to($('#banner'), 0.5, {opacity: 0})
     .to($('#ferris'), 0.5, {bottom: '-100%'}, '-=0.1')
     .to($('#first .fourth'), 0.5, {bottom: 0, ease: Expo.easeOut}, '-=0.2')
     .to($('#first .third'), 0.5, {opacity: 1, bottom: 0, ease: Expo.easeOut}, '-=0.2')
     .to($('#first .second'), 0.5, {opacity: 1, bottom: 0, ease: Expo.easeOut}, '-=0.2')
     .to($('#first .first'), 0.5, {opacity: 1, bottom: 0, ease: Expo.easeOut}, '-=0.2')
     .to($('#first .transition'), 0.5, {bottom: '-15vh', ease: Expo.easeIn})
-    .to(window, 0.5, {scrollTo: '#second', autoKill: false})
   ).addTo(c);
 
   // second scene: shrink box
@@ -45,9 +43,21 @@ $(() => {
   new ScrollMagic.Scene({
     triggerElement: '#second',
     duration: 0
-  }).setTween('#second .box', 0.2, {top: '5vh', left: '5vw', right: '5vw'})
-    .addTo(c)
+  }).setTween('#second .box', 0.2, {
+    top: '5vh', left: '5vw', right: '5vw', onReverseComplete: jump('#first', true)
+  }).addTo(c)
     .addIndicators();
 
   // first page transition
 });
+
+var tw = TweenMax;
+var tl = TimelineMax;
+
+// helpers
+function jump(target, reverse) {
+  return () => {
+    seek = new tl().to(window, 0.5, {scrollTo: target, autoKill: false});
+    return reverse ? seek.to(window, 0, {scrollTo: {y: -1}, autoKill: false}): seek;
+  }
+}
