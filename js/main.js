9ded88a35e3a5377f344265ee577a9165a84de7c
Kenneth Ma <ken@kenma.ca>
2017-05-02 00:37:49 -0400

    Added: More WIP to first transition to have more natural animation

--- js/main.js
+++ js/main.js
@@ -13,16 +13,15 @@ $(() => {
   var tw = TweenMax;
   var tl = TimelineMax;
 
-  // always in motion: ferris wheel
+  // first scene: ferris wheel rotation
   tw.to($('g[class*="wheel"], #cars'), FERRIS_SPEED, {rotation: ROTATION, repeat: -1, transformOrigin: '50% 50%', ease: Linear.easeNone});
   tw.to($('.car'), FERRIS_SPEED, {rotation: -ROTATION, repeat: -1, transformOrigin: '50% 50%', ease: Linear.easeNone});
 
-  // first scene
+  // first scene: cloud transition
   new ScrollMagic.Scene({
     triggerElement: '#first',
     duration: '10%'
   }).setPin('#first').addTo(c);
-
   new ScrollMagic.Scene({
     triggerElement: '#first',
     duration: 0,
@@ -30,25 +29,25 @@ $(() => {
   }).setTween(new tl()
     .to($('#banner'), 0.5, {opacity: 0})
     .to($('#ferris'), 0.5, {bottom: '-100%'}, '-=0.1')
-    .to($('#first .fourth'), 0.2, {bottom: 0, ease: Expo.easeOut})
-    .to($('#first .third'), 0.2, {opacity: 1, bottom: 0, ease: Expo.easeOut})
-    .to($('#first .second'), 0.2, {opacity: 1, bottom: 0, ease: Expo.easeOut})
-    .to($('#first .first'), 0.2, {opacity: 1, bottom: 0, ease: Expo.easeOut})
-    .to(window, 0.5, {scrollTo: '#second', autoKill: false, ease: Expo.easeOut})
+    .to($('#first .fourth'), 0.5, {bottom: 0, ease: Expo.easeOut}, '-=0.2')
+    .to($('#first .third'), 0.5, {opacity: 1, bottom: 0, ease: Expo.easeOut}, '-=0.2')
+    .to($('#first .second'), 0.5, {opacity: 1, bottom: 0, ease: Expo.easeOut}, '-=0.2')
+    .to($('#first .first'), 0.5, {opacity: 1, bottom: 0, ease: Expo.easeOut}, '-=0.2')
+    .to($('#first .transition'), 0.5, {bottom: '-15vh', ease: Expo.easeIn})
+    .to(window, 0.5, {scrollTo: '#second', autoKill: false})
   ).addTo(c);
 
-  // second scene
+  // second scene: shrink box
+  new ScrollMagic.Scene({
+    triggerElement: '#second',
+    duration: '10%'
+  }).setPin('#second').addTo(c);
   new ScrollMagic.Scene({
     triggerElement: '#second',
     duration: 0
   }).setTween('#second .box', 0.2, {top: '5vh', left: '5vw', right: '5vw'})
     .addTo(c)
     .addIndicators();
-
-  new ScrollMagic.Scene({
-    triggerElement: '#second',
-    duration: '10%'
-  }).setPin('#second').addTo(c);
 
   // first page transition
 });
