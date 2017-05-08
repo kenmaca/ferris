3d774275df2994e9499855104541c2a127343951
Kenneth Ma <ken@kenma.ca>
2017-05-08 13:07:14 -0400

    Added: Fifth scene, and positioning of swings, overlapping and scrolling with fourth and fifth screens

---
+++ js/scenes/fifth.js
@@ -0,0 +1,8 @@
+
+/* Fourth Scene animations and sprites.
+   @author Kenneth Ma (http://kenma.ca), 2017
+*/
+
+$(() => {
+  shrinkScene('#fourth', '#fifth', c);
+});
--- js/scenes/fourth.js
+++ js/scenes/fourth.js
@@ -8,4 +8,31 @@ loadSvg('swings', '#swings');
 
 $(() => {
   shrinkScene('#third', '#fourth', c);
+
+  // scroll to swings
+  new ScrollMagic.Scene({
+
+    // all scenes (scene * number of scenes)
+    triggerElement: '#first',
+    duration: $('#fourth').height() * 5
+  }).setTween('#swings', {
+
+    // adjusts the amount of scroll proportionate to scene
+    bottom: $('#fourth').height() * -0.1
+  }).addIndicators().addTo(c);
+
+  // cloud transition
+  new ScrollMagic.Scene({
+    triggerElement: '#fourth',
+    duration: 0,
+    offset: 1
+  }).setTween(new tl({
+    onComplete: jump('#fifth')
+  }).to($('#fourth .content'), 0.2, {opacity: 0})
+    .to($('#fourth .transition .first'), 0.5, {opacity: 1, bottom: 0, ease: Expo.easeOut}, '-=0.2')
+    .to($('#fourth .transition .second'), 0.5, {opacity: 1, bottom: 0, ease: Expo.easeOut}, '-=0.2')
+    .to($('#fourth .transition .third'), 0.5, {opacity: 1, bottom: 0, ease: Expo.easeOut}, '-=1')
+    .to($('#fourth .transition .fourth'), 0.5, {opacity: 1, bottom: 0, ease: Expo.easeOut}, '-=0.8')
+    .to($('#fourth .transition'), 0.2, {bottom: -50, ease: Linear.easeNone})
+  ).addTo(c);
 });
