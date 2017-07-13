3339189766b3e8b62fbd132e4248d5b9b27f9aaa
Roleen Nunes <newnessroleen@gmail.com>
2017-07-13 02:04:34 -0400

    Completed fifth page

--- js/scenes/fifth.js
+++ js/scenes/fifth.js
@@ -5,6 +5,16 @@
 
 // sprites
 loadSvg('cat-tent');
+loadSvg('path-tent');
+loadSvg('sharpener');
+loadSvg('bushes/bush-1', '#bush-1');
+loadSvg('bushes/bush-2', '#bush-2');
+loadSvg('bushes/bush-3', '#bush-3');
+loadSvg('bushes/bush-4', '#bush-4');
+loadSvg('bushes/bush-5', '#bush-5');
+loadSvg('bushes/bush-6', '#bush-6');
+loadSvg('bushes/bush-7', '#bush-7');
+loadSvg('bushes/bush-8', '#bush-8');
 
 $(() => {
 });
