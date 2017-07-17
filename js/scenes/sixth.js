5f3301f3c60cf39950ef2db715200c88c0743148
Kenneth Ma <ken@kenma.ca>
2017-07-16 21:58:53 -0400

    Added content to fifth page

---
+++ js/scenes/sixth.js
@@ -0,0 +1,24 @@
+
+/* Sixth Scene animations and sprites.
+   @author Kenneth Ma (http://kenma.ca), 2017
+*/
+
+// sprites
+loadSvg('cat-tent');
+loadSvg('path-tent');
+loadSvg('sharpener');
+loadSvg('bushes/bush-1', '#bush-1');
+loadSvg('bushes/bush-2', '#bush-2');
+loadSvg('bushes/bush-3', '#bush-3');
+loadSvg('bushes/bush-4', '#bush-4');
+loadSvg('bushes/bush-5', '#bush-5');
+loadSvg('bushes/bush-6', '#bush-6');
+loadSvg('bushes/bush-7', '#bush-7');
+loadSvg('bushes/bush-8', '#bush-8');
+
+// clouds
+loadSvg('clouds/cloud-13', '#cloud-13');
+loadSvg('clouds/cloud-14', '#cloud-14');
+
+$(() => {
+});
