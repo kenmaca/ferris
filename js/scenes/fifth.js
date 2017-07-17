67f9ce96615f93fefe55409e9a1ae4fca629d014
Kenneth Ma <ken@kenma.ca>
2017-07-16 20:38:27 -0400

    Corrected fourth scene and added sixth

--- js/scenes/fifth.js
+++ js/scenes/fifth.js
@@ -1,20 +1,7 @@
 
-/* Fourth Scene animations and sprites.
+/* Fifth Scene animations and sprites.
    @author Kenneth Ma (http://kenma.ca), 2017
 */
-
-// sprites
-loadSvg('cat-tent');
-loadSvg('path-tent');
-loadSvg('sharpener');
-loadSvg('bushes/bush-1', '#bush-1');
-loadSvg('bushes/bush-2', '#bush-2');
-loadSvg('bushes/bush-3', '#bush-3');
-loadSvg('bushes/bush-4', '#bush-4');
-loadSvg('bushes/bush-5', '#bush-5');
-loadSvg('bushes/bush-6', '#bush-6');
-loadSvg('bushes/bush-7', '#bush-7');
-loadSvg('bushes/bush-8', '#bush-8');
 
 // clouds
 loadSvg('clouds/cloud-13', '#cloud-13');
